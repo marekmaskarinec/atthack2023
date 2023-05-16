561261aa6239c20ce946b6a76bd09fb7bb4486e9
Marek Maškarinec <marek@mrms.cz>
2023-05-16 21:50:41 +0200

    stuff

--- src/move.h
+++ src/move.h
@@ -5,6 +5,7 @@
 
 bool *_move_path;
 bool *_move_path_end;
+int _move_swc;
 
 void move_loop();
 String move_log();
@@ -21,53 +22,24 @@ void move_loop() {
 	const int r = digitalRead(DEV_LINE_R);
 	const int c = l + m + r;
 
-	const int speed = 255;
-		/*if (l) dev_set_speed(60, 255);
-		else if (r) dev_set_speed(255, 60);*/
-		if (l) dev_set_speed(0, 0);
-		else if (r) dev_set_speed(0, 0);
-		else dev_set_speed(speed, speed);
-	/*switch (c) {
-	case 0:
-		dev_set_speed(255, -255);
-		break;
-	case 1:
-		if (l) dev_set_speed(128, 255);
-		else if (r) dev_set_speed(255, 128);
-		else dev_set_speed(255, 255);
-		break;
-	case 2: { // this is hacky at best
-		if (_move_path == _move_path_end)
-			break;
-		const bool p = *_move_path;
-		++_move_path;
-		if (p) {
-			dev_set_speed(255, 255);
-			delay(50);
-			break;
-		}
+	if (c == 0) {
+		dev_set_speed(0, 0);
+		return;
+	}
 
-		if (l) {
-			dev_set_speed(0, 255);
-			delay(50);
-		} else if (r) {
-			dev_set_speed(255, 0);
-			delay(50);
+	if (l && m || r && m) {
+		// horrible cycle delta dependant stuff TODO XXX FIXME
+		if (++_move_swc > 10) {
+			dev_set_speed(0, 0);
+			delay(500);
 		}
+	} else {
+		_move_swc = 0;
+	}
 
-		dev_set_speed(255, 255);
-		delay(50);
-		break;
-	} case 3:
-		digitalWrite(LED_BUILTIN, 1);	
-		delay(200);
-		digitalWrite(LED_BUILTIN, 0);	
-		delay(200);
-		digitalWrite(LED_BUILTIN, 1);	
-		delay(200);
-		digitalWrite(LED_BUILTIN, 0);	
-		break;
-	}*/
+	if (l) dev_set_speed(0, 150);
+	else if (r) dev_set_speed(150, 0);
+	else dev_set_speed(150, 150);
 }
 
 String move_log() {
